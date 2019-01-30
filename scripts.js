fe8fc6cde4cc6c1ffd42ff6ddd856826b8d6c836
polik67 <46492348+polik67@users.noreply.github.com>
2019-01-29 22:35:23 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,66 @@
+// Interacting
+ 
+var p1 = document.querySelector('.p1')
+
+
+p1.addEventListener('click', rotate)
+
+
+
+
+function rotate() {
+	console.log('it works')
+	
+	if (p1.id != 'rotate') {
+		p1.id = 'rotate'
+	} else {
+		p1.id = ''
+	}
+
+}
+
+
+var p2 = document.querySelector('.p2')
+
+
+p2.addEventListener('click', zoom)
+
+
+
+
+function zoom() {
+	console.log('it works')
+	
+	if (p2.id != 'zoom') {
+		p2.id = 'zoom'
+	} else {
+		p2.id = ''
+	}
+
+}
+
+
+var p3 = document.querySelector('.p3')
+
+
+p3.addEventListener('click', move)
+
+
+
+
+function move() {
+	console.log('it works')
+	
+	if (p3.id != 'move') {
+		p3.id = 'move'
+	} else {
+		p3.id = ''
+	}
+
+}
+
+
+
+
+
+
